88ca4fff77d0ee7cfd5b259b2a555cd72edc98fa
TRAIN 56 <TRAIN 56@LAPTOP-H76H9HGS>
2021-09-27 13:11:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string stringVar = "Hello World!!";
+			int intVar = 100;
+			float floatVar = 10.2f;
+			char charVar = 'A';
+			bool boolVar = true;
+
+			Console.WriteLine(stringVar);
+			Console.WriteLine(intVar);
+			Console.WriteLine(floatVar);
+			Console.WriteLine(charVar);
+			Console.WriteLine(boolVar);
+		}
+    }
+}
